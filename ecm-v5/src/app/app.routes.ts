20360df3fca92673005feebcfb86bb91bf5cc850
Mags8934 <maggie.werning@gmail.com>
2024-04-07 18:00:37 -0500

    about routes

--- ecm-v5/src/app/app.routes.ts
+++ ecm-v5/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '', redirectTo: '/home', pathMatch: 'full'},
+{path: 'about', component: AboutComponent},
+{ path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
