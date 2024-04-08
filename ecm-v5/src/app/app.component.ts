3cc4d40ff7d385b56a7bb1ff036945ad521f739f
Mags8934 <maggie.werning@gmail.com>
2024-04-07 19:58:56 -0500

    home component

--- ecm-v5/src/app/app.component.ts
+++ ecm-v5/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { AboutComponent } from './about/about.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- ecm-v5/src/app/app.routes.ts
+++ ecm-v5/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-{path: '', redirectTo: '/about', pathMatch: 'full'},
+{path: '', redirectTo: '/home', pathMatch: 'full'},
+{path: 'home' , component: HomeComponent},
 {path: 'about', component: AboutComponent},
-{ path: '**', redirectTo: '/about', pathMatch: 'full'}
+{ path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
