3e3f5639ce69f77eb910289829f50ca3c6c38bfc
Mags8934 <maggie.werning@gmail.com>
2024-04-08 00:12:21 -0500

    component update

--- ecm-v5/src/app/app.component.ts
+++ ecm-v5/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { NavComponent } from "./nav/nav.component";
 import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from './home/home.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavComponent, FooterComponent]
+    imports: [RouterOutlet, NavComponent, FooterComponent, AboutComponent, HomeComponent, ]
 })
 export class AppComponent {
   title = 'ecm-v5';
